4850507ee8d566b3711361331e6ca90dc8bd2392
Python_Pioneer <89539411+Tonyjr7@users.noreply.github.com>
2025-02-01 00:30:04 +0000

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import ME
+from django.utils import timezone
 
 class MESerializer(serializers.ModelSerializer):
+
+     current_datetime = serializers.SerializerMethodField()
+    
     class Meta:
         model = ME
         fields = ['email', 'current_datetime', 'github_url']
+
+ def get_current_datetime(self, obj):
+        return timezone.now()
