3895622e7fff8167e3742f313da6cb63b1f49407
Python_Pioneer <89539411+Tonyjr7@users.noreply.github.com>
2025-02-01 00:22:41 +0000

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from django.utils import timezone
 from rest_framework import generics
 from .models import ME
 from api.serializers import MESerializer
@@ -8,6 +9,18 @@ from api.serializers import MESerializer
 class ListME(generics.ListAPIView):
     queryset = ME.objects.all()
     serializer_class = MESerializer
+
+    def list(self, request, *args, **kwargs):
+        # Get the current datetime for every request
+        current_time = timezone.now()
+
+        # Get the default response data
+        response = super().list(request, *args, **kwargs)
+        
+        # Add the current time to the response data
+        response.data.append({"current_datetime": current_time})  # Add real-time current time to the response
+
+        return response
     
 
 
